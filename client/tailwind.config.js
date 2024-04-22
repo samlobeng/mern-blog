2bbc78f29cb2f2901c94c4dd81e7d0782be59e17
Samuel Obeng <samuelobeng2006@gmail.com>
2024-04-22 17:19:09 +0200

    Created the header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,17 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat",],
+      },
+    },
   },
-  plugins: [],
+  plugins: [flowbite.plugin(),],
 }
