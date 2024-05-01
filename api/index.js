824141d3bc0ee5c400c22fa66c079aba1b9a9917
Samuel Obeng <samuelobeng2006@gmail.com>
2024-05-01 18:24:33 +0200

    Added functionality to the sign up page

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use('/api/auth', authRoutes)
 
 app.use((err, req, res, next)=>{
     const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error"
+    const message = 'User already exists' || "Internal Server Error"
     res.status(statusCode).json({
         success: false,
         statusCode,
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,49 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignUp() {
+const [formData, setFormData] = useState({})
+const [errorMessage, setErrorMessage] = useState(null)
+const [isLoading, setIsLoading] = useState(false)
+const [showAlert, setShowAlert] = useState(true)
+const navigate = useNavigate()
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value.trim()})
+    console.log(formData)
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    if(!formData.username || !formData.password || !formData.email){
+      setShowAlert(true);
+      return setErrorMessage("Please fill out all fields")
+      
+    }
+    // send form data to server here
+    try {
+      setIsLoading(true)
+      setErrorMessage(null)
+        const res = await fetch('/api/auth/signup', {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(formData),
+        })
+        const data = await res.json()
+        if(data.success===false){
+          setShowAlert(true);
+          setIsLoading(false)
+          return setErrorMessage(data.message)
+        }
+        if(res.ok){
+          return navigate('/sign-in')
+        }
+    } catch (error) {
+      setErrorMessage(error.message)
+      setIsLoading(false)
+    }
+
+  }
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-6xl mx-auto flex-col md:flex-row gap-5'>
@@ -18,24 +59,38 @@ export default function SignUp() {
         </p>
         </div>
         <div className="flex-1">
-          <form className='flex flex-col gap-5'>
+          <form className='flex flex-col gap-5' onSubmit={handleSubmit}>
             <div>
             <Label value='Your Username'/>
-            <TextInput type = "text" placeholder='Username' id='username' />
+            <TextInput type = "text" placeholder='Username' id='username' onChange={handleChange}/>
             </div>
             <div>
             <Label value='Your Email'/>
-            <TextInput type = "text" placeholder='name@qrinnyverse.com' id='email' />
+            <TextInput type = "email" placeholder='name@qrinnyverse.com' id='email' onChange={handleChange} />
             </div>
             <div>
             <Label value='Your Password'/>
-            <TextInput type = "password" placeholder='Password' id='password' />
+            <TextInput type = "password" placeholder='Password' id='password'onChange={handleChange} />
             </div>
-            <Button type = "submit" className='bg-teal-500'>Sign Up</Button>
+            <Button type = "submit" className='bg-teal-500' disabled={isLoading}>
+              {
+                isLoading ? (<>
+                  <Spinner size = 'sm'/>
+                  <span className='pl-3'>Loading...</span>
+                  </>): 'Sign Up'
+              }
+              </Button>
           </form>
           <div className="mt-5">
             Already have an account? &nbsp;<Link to="/sign-in" className='text-blue-500'>Sign in</Link>
           </div>
+          {
+            errorMessage && showAlert &&(
+              <Alert className='mt-5' color='failure' onDismiss = {()=>setShowAlert(false) }>
+                {errorMessage}
+              </Alert>
+            )
+          }
         </div>
       </div>
     </div>
