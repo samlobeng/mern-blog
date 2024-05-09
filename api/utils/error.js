1835f591755cfe1f6534a1699bdefba5b4953552
Samuel Obeng <samuelobeng2006@gmail.com>
2024-05-09 18:09:03 +0200

    Created signin api route

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,6 @@
 export const errorHandler = (statusCode, message)=>{
     const error = new Error();
-    error.httpStatusCode = statusCode;
+    error.statusCode = statusCode;
     error.message = message;
     return error;
 }
